5744b6d0278cabfc8aa21132fba77f6bf3661e46
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-03 13:51:41 -0600

    get_contestant_name

---
+++ leetcode-challenges-ruby/spec/winning_season.rb
@@ -0,0 +1,51 @@
+
+data = {
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+}
+
+def get_contestant_name(data, occupation)
+  winners = []
+   data.each do |season, array|
+    array.each do |people|
+        people.each do |key, value|
+          if value == "Winner"
+            winners.push(people[:name])
+          end
+        end
+      end
+    end
+    return winners
+end
+
+get_contestant_name(data, "Entertainer")
